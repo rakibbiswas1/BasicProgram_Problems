af0127af15e65192778eea4c83ae9b51e519fb7a
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-21 15:23:58 +0530

    checking factor number

---
+++ src/Factors.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Factors {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int a = sc.nextInt();
+        System.out.println("Factors of " + a + " are ");
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
