f31b57d3fc1bf64a1632b8ae38474c42bc0c3b06
rakibbiswas1 <rakibbiswas.official@gmail.com>
2022-08-21 16:19:39 +0530

    checking vowel consonant alphabet

---
+++ src/Vowel_Consonant.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Vowel_Consonant {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter an alphabet: ");
+        char c = sc.next().charAt(0);
+        if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+        {
+            System.out.println(c + " is a vowel ");  // condition true input is vowel
+        }
+        else
+        {
+            System.out.println(c + " is a consonant ");  // condition true input is consonant
+        }
+    }
+}
